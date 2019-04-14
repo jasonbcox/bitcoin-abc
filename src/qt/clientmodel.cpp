bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Remove direct bitcoin calls from qt/peertablemodel.cpp

src/qt/clientmodel.cpp
@@ -40,7 +40,7 @@ ClientModel::ClientModel(ipc::Node& _ipcNode, OptionsModel *_optionsModel, QObje
 {
     cachedBestHeaderHeight = -1;
     cachedBestHeaderTime = -1;
-    peerTableModel = new PeerTableModel(this);
+    peerTableModel = new PeerTableModel(ipcNode, this);
     banTableModel = new BanTableModel(this);
     pollTimer = new QTimer(this);
     connect(pollTimer, SIGNAL(timeout()), this, SLOT(updateTimer()));

jasonbcox/bitcoin-abc

src/qt/clientmodel.cpp
@@ -38,7 +38,7 @@ ClientModel::ClientModel(interface::Node &node, OptionsModel *_optionsModel, QOb
       banTableModel(0), pollTimer(0) {
     cachedBestHeaderHeight = -1;
     cachedBestHeaderTime = -1;
-    peerTableModel = new PeerTableModel(this);
+    peerTableModel = new PeerTableModel(m_node, this);
     banTableModel = new BanTableModel(this);
     pollTimer = new QTimer(this);
     connect(pollTimer, SIGNAL(timeout()), this, SLOT(updateTimer()));